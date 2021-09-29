fadac5997b08298df594403dc83aeec867b66ae6
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-09-29 14:36:58 +0530

    Added working Protobuf serialization and deserialization in memory.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Protobuf
-Playing with Protobufs with C#
+
+* Navigate to the protobuf from GitHub [`releases page`](https://github.com/protocolbuffers/protobuf/releases/latest) and download the `protoc-<VERSION>-win64.zip` for Windows, this includes a pre-built binary of the protoc tool, compiler etc.
+
+* Extract it, place it in some folder like `C:\` and add the directory `C:\protoc-<VERSION>-win64\bin` to the path variable.
+
+* Verify whether added properly to path variable or not by typing in command prompt - `protoc -help`
+
+* Follow Google [tutorial](https://developers.google.com/protocol-buffers/docs/csharptutorial) for Protobuf using C#.
+
+* Compile the .proto file to C# class
+    >  protoc -I. --csharp_out=. ./addressbook.proto
+    "This generates Addressbook.cs in the solution folder".
+
+* To compile this code, we need to reference the [`Google.Protobuf`](https://www.nuget.org/packages/google.protobuf) assembly.
+    > PM > Install-Package Google.Protobuf
+
+* Add using directives to Program.cs for generated class form compiler (if namespace if different), and `Google.Protobuf`.
---
+++ src/Protobuf/Program.cs
@@ -0,0 +1,42 @@
+﻿using Google.Protobuf;
+using System.Collections.Generic;
+using static Protobuf.Person.Types;
+
+namespace Protobuf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person jesus = new()
+            {
+                Id = 1234,
+                Name = "Jesus",
+                Email = "jesus@jerusalem.com",
+                Phones =
+                {
+                    new PhoneNumber
+                    {
+                        Number = "1234567890",
+                        Type = PhoneType.Mobile
+                    }
+                }
+            };
+
+            // Collections are get-only, use .Add to mutate the list
+            // See https://stackoverflow.com/a/16618870/7177029
+            jesus.Aliases.Add(new[] { "Water walker", "Wine maker" });
+            jesus.Kvs.Add(new Dictionary<string, string>
+            {
+                ["WineMaker"] = "Jesus",
+                ["WaterWalker"] = "Jesus"
+            });
+
+            // Serialize to byte array
+            byte[] b = jesus.ToByteArray();
+
+            // Deserialize back
+            Person jesusResurrected = Person.Parser.ParseFrom(b);
+        }
+    }
+}
